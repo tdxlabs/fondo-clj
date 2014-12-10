d1b12c00c972448b2da81cc2c829c07a8929de92
David James <david@bluemontlabs.com>
2014-12-10 17:23:28 -0500

    WIP: start a Fondo node with `lein ring server`

--- project.clj
+++ project.clj
@@ -6,6 +6,17 @@
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [org.clojure/data.codec "0.1.0"]
                  [com.amazonaws/aws-java-sdk "1.9.10" :exclusions [joda-time]]
-                 [com.cognitect/transit-clj "0.8.259"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
-                   :source-paths ["dev"]}})
+                 [com.cognitect/transit-clj "0.8.259"]
+                 [ring/ring-core "1.3.1"
+                  :exclusions [commons-codec]]
+                 [ring/ring-jetty-adapter "1.3.1"]
+                 [ring/ring-json "0.3.1"]
+                 [cheshire "5.4.0"]]
+  :plugins [[lein-ring "0.8.11"
+             :exclusions [org.clojure/clojure
+                          org.clojure/data.xml]]]
+  :ring {:handler fondo.core/node-app}
+  :profiles
+  {:dev
+   {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
+    :source-paths ["dev"]}})
--- src/fondo/core.clj
+++ src/fondo/core.clj
@@ -1,1 +1,42 @@
-(ns fondo.core)
+(ns fondo.core
+  (:require
+   [cheshire.core :as json]
+   [compojure.core :refer [routes GET]]
+   [compojure.route :as route]
+   [ring.middleware.content-type :refer [wrap-content-type]]
+   [ring.middleware.json :refer [wrap-json-response]]
+   [ring.middleware.params :refer [wrap-params]]
+   [ring.util.response :refer [response]]
+   ))
+
+(comment (clojure.tools.reader.edn/read))
+
+(def zone-id "12345")
+
+(defn node-root
+  [request]
+  {:status 200
+   :headers {}
+   :body {:API ["GET /info"]}})
+
+(defn node-info
+  [request]
+  {:status 200
+   :headers {}
+   :body {:fondo-version "0.1.0"
+          :server ["fondo-clj" "0.1.0"]
+          :zone-id zone-id}})
+
+(def node-routes
+  (routes
+   (GET "/" [] node-root)
+   (GET "/info" [] node-info)))
+
+(def node-app
+  (-> node-routes
+      wrap-json-response))
+
+;; FUTURE:
+;; * use DynamoDB
+;; * add authentication
+
