817adfe6565e62528a8b2c4f9397abed83b8d912
Joshua Miller <josh@joshmiller.io>
2014-12-22 10:36:24 -0700

    Test storing/retrieving values.

--- src/fondo/db.clj
+++ src/fondo/db.clj
@@ -8,14 +8,15 @@
    :secret-key "<AWS_DYNAMODB_SECRET_KEY>"
    :endpoint "http://localhost:8000"})
 
+(def default-table-name :values)
+
 (defn initialize-table
   "Set up Dynamo table to store Fondo values"
-  []
+  [& [table-name]]
   (far/create-table dynamodb
-                    :values
+                    (or table-name default-table-name)
                     [:vid :n]
                     {}))
-
 
 (def value-validations
   {:name [v/string v/required]
@@ -23,19 +24,21 @@
 
 ;; TODO: Ensure no current value with `id` is stored
 (defn put-value
-  [id val]
+  [id val & [table-name]]
   (let [e (v/errors val value-validations)]
     (if (empty? e)
       (do
         (far/put-item dynamodb
-                      :values
+                      (or table-name default-table-name)
                       {:vid id
                        :value (far/freeze val)})
         {:stored true :vid id})
       {:errors e})))
 
 (defn get-value
-  [id]
-  (if-let [val (:value (far/get-item dynamodb :values {:vid id}))]
+  [id & [table-name]]
+  (if-let [val (:value (far/get-item dynamodb
+                                     (or table-name default-table-name)
+                                     {:vid id}))]
     {:id id :value val}
     {:error :not-found}))
--- test/fondo/db_test.clj
+++ test/fondo/db_test.clj
@@ -1,7 +1,17 @@
 (ns fondo.db-test
   (:require [clojure.test :refer :all]
             [fondo.db :as db]
+            [taoensso.faraday :as far]
             [validata.core :as v]))
+
+(defn table-setup
+  "Create table, then destroy on completion"
+  [f]
+  (db/initialize-table :test-values)
+  (f)
+  (far/delete-table db/dynamodb :test-values))
+
+(use-fixtures :once table-setup)
 
 (deftest validate-value
   (testing "correctly validates values"
@@ -16,3 +26,15 @@
       (is (v/valid? valid2 db/value-validations))
       (is (not (v/valid? invalid1 db/value-validations)))
       (is (not (v/valid? invalid2 db/value-validations))))))
+
+(deftest put-and-get-values
+  (testing "stores and retrieves values"
+    (let [id 12345
+          val {:name "Test"
+               :uri "URI"}]
+      (db/put-value id val :test-values)
+      (let [result (db/get-value id :test-values)
+            missing (db/get-value 67890 :test-values)]
+        (is (= id (:id result)))
+        (is (= val (:value result)))
+        (is (= {:error :not-found} missing))))))
